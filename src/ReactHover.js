d1a0349e40950d461251ad6e71a76236255f58cc
natanweinberger <naweinberger+github@gmail.com>
2017-07-19 19:14:41 -0400

    Add containerStyle prop to ReactHover

--- src/ReactHover.js
+++ src/ReactHover.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
-import Hover from './lib/Hover'
-import Trigger from './lib/Trigger'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import Hover from './lib/Hover';
+import Trigger from './lib/Trigger';
+import PropTypes from 'prop-types';
 
 class ReactHover extends Component {
   static propTypes = {
     children: PropTypes.array.isRequired,
     options: PropTypes.object.isRequired,
-    className: PropTypes.string
-  }
+    className: PropTypes.string,
+    containerStyle: PropTypes.object,
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       children: PropTypes.object,
       hoverComponentStyle: {
         display: 'none',
-        position: 'absolute'
-      }
-    }
+        position: 'absolute',
+      },
+    };
   }
 
   renderItem(item, index) {
@@ -27,71 +28,80 @@ class ReactHover extends Component {
         <Trigger key={index}>
           {item}
         </Trigger>
-      )
+      );
     } else if (item.type.name == 'Hover') {
       return (
         <Hover key={index}>
           {item}
         </Hover>
-      )
+      );
     }
   }
 
   render() {
-    const { hoverComponentStyle } = this.state
+    const { containerStyle } = this.props;
+    const { hoverComponentStyle } = this.state;
     let childrenWithProps = [];
     for (let child of this.props.children) {
       if (child.type.name == 'Trigger') {
-        childrenWithProps.push(React.cloneElement(child, {
-          setVisibility: this.setVisibility.bind(this),
-          getCursorPos: this.getCursorPos.bind(this)
-        }));
+        childrenWithProps.push(
+          React.cloneElement(child, {
+            setVisibility: this.setVisibility.bind(this),
+            getCursorPos: this.getCursorPos.bind(this),
+          })
+        );
       } else if (child.type.name == 'Hover') {
-        childrenWithProps.push(React.cloneElement(child, {
-          styles: hoverComponentStyle
-        }));
+        childrenWithProps.push(
+          React.cloneElement(child, {
+            styles: hoverComponentStyle,
+          })
+        );
       }
     }
 
     return (
-      <div>
+      <div style={containerStyle}>
         {childrenWithProps.map((item, index) => this.renderItem(item, index))}
       </div>
-    )
+    );
   }
 
   setVisibility(flag) {
-    let { hoverComponentStyle } = this.state
-    let updatedStyles = null
+    let { hoverComponentStyle } = this.state;
+    let updatedStyles = null;
     if (flag) {
-      updatedStyles = { ...hoverComponentStyle, display: 'block' }
+      updatedStyles = { ...hoverComponentStyle, display: 'block' };
     } else {
-      updatedStyles = { ...hoverComponentStyle, display: 'none' }
+      updatedStyles = { ...hoverComponentStyle, display: 'none' };
     }
     this.setState({
-      hoverComponentStyle: updatedStyles
-    })
+      hoverComponentStyle: updatedStyles,
+    });
   }
 
   getCursorPos(e) {
-    const cursorX = e.pageX
-    const cursorY = e.pageY
-    let { options: { followCursor, shiftX, shiftY } } = this.props
-    let { hoverComponentStyle } = this.state
-    let updatedStyles = null
+    const cursorX = e.pageX;
+    const cursorY = e.pageY;
+    let { options: { followCursor, shiftX, shiftY } } = this.props;
+    let { hoverComponentStyle } = this.state;
+    let updatedStyles = null;
     if (!followCursor) {
-      return
+      return;
     }
     if (isNaN(shiftX)) {
-      shiftX = 0
+      shiftX = 0;
     }
     if (isNaN(shiftY)) {
-      shiftY = 0
+      shiftY = 0;
     }
-    updatedStyles = { ...hoverComponentStyle, top: cursorY + shiftY, left: cursorX + shiftX }
+    updatedStyles = {
+      ...hoverComponentStyle,
+      top: cursorY + shiftY,
+      left: cursorX + shiftX,
+    };
     this.setState({
-      hoverComponentStyle: updatedStyles
-    })
+      hoverComponentStyle: updatedStyles,
+    });
   }
 }
 
